b4c80d3be2c067c1d3dc15e1358853b08755ace5
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-05-01 14:03:18 +0300

    make_utc_tm (Howard Hinnon) added

--- time.h
+++ time.h
@@ -1,6 +1,30 @@
-// -*-coding: mule-utf-8-unix; fill-column: 58; -*-
+/* -*-coding: mule-utf-8-unix; fill-column: 58; -*-
+
+  Copyright (C) 2009, 2013 Sergei Lodyagin 
+ 
+  This file is part of the Cohors Concurro library.
+
+  This library is free software: you can redistribute it
+  and/or modify it under the terms of the GNU Lesser
+  General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * @file
+ * The implementation of std::put_time (it's absent in
+ * GCC) in the Bare C++ style.
  *
  * @author Sergei Lodyagin
  * @copyright Copyright (C) 2013 Cohors LLC 
@@ -13,12 +37,145 @@
 #include <ratio>
 #include <ctime>
 #include <iomanip>
-//#include "conversion.h"
 
 #ifndef CONCURRO_TYPES_TIME_H_
 #define CONCURRO_TYPES_TIME_H_
 
 namespace curr {
+
+namespace time {
+
+namespace howard_hinnant {
+
+//! Returns number of days since civil 1970-01-01.  Negative values indicate
+//!    days prior to 1970-01-01.
+//! Preconditions:  y-m-d represents a date in the civil (Gregorian) calendar
+//!                 m is in [1, 12]
+//!                 d is in [1, last_day_of_month(y, m)]
+//!                 y is "approximately" in
+//!                   [numeric_limits<Int>::min()/366, numeric_limits<Int>::max()/366]
+//!                 Exact range of validity is:
+//!                 [civil_from_days(numeric_limits<Int>::min()),
+//!                  civil_from_days(numeric_limits<Int>::max()-719468)]
+//! Howard Hinnant
+//! chrono-Compatible Low-Level Date Algorithms
+//! http://home.roadrunner.com/~hinnant/date_algorithms.html
+template <class Int>
+// constexpr c++14
+Int
+days_from_civil(Int y, unsigned m, unsigned d) noexcept
+{
+    static_assert(std::numeric_limits<unsigned>::digits >= 18,
+             "This algorithm has not been ported to a 16 bit unsigned integer");
+    static_assert(std::numeric_limits<Int>::digits >= 20,
+             "This algorithm has not been ported to a 16 bit signed integer");
+    y -= m <= 2;
+    const Int era = (y >= 0 ? y : y-399) / 400;
+    const unsigned yoe = static_cast<unsigned>(y - era * 400);      // [0, 399]
+    const unsigned doy = (153*(m + (m > 2 ? -3 : 9)) + 2)/5 + d-1;  // [0, 365]
+    const unsigned doe = yoe * 365 + yoe/4 - yoe/100 + doy;         // [0, 146096]
+    return era * 146097 + static_cast<Int>(doe) - 719468;
+}
+
+//! Returns year/month/day triple in civil calendar
+//! Preconditions:  z is number of days since 1970-01-01 and is in the range:
+//!                   [numeric_limits<Int>::min(), numeric_limits<Int>::max()-719468].
+//! Howard Hinnant
+//! chrono-Compatible Low-Level Date Algorithms
+//! http://home.roadrunner.com/~hinnant/date_algorithms.html
+template <class Int>
+//constexpr (c++14)
+std::tuple<Int, unsigned, unsigned> 
+civil_from_days(Int z) noexcept
+{
+    static_assert(std::numeric_limits<unsigned>::digits >= 18,
+             "This algorithm has not been ported to a 16 bit unsigned integer");
+    static_assert(std::numeric_limits<Int>::digits >= 20,
+             "This algorithm has not been ported to a 16 bit signed integer");
+    z += 719468;
+    const Int era = (z >= 0 ? z : z - 146096) / 146097;
+    const unsigned doe = static_cast<unsigned>(z - era * 146097);          // [0, 146096]
+    const unsigned yoe = (doe - doe/1460 + doe/36524 - doe/146096) / 365;  // [0, 399]
+    const Int y = static_cast<Int>(yoe) + era * 400;
+    const unsigned doy = doe - (365*yoe + yoe/4 - yoe/100);                // [0, 365]
+    const unsigned mp = (5*doy + 2)/153;                                   // [0, 11]
+    const unsigned d = doy - (153*mp+2)/5 + 1;                             // [1, 31]
+    const unsigned m = mp + (mp < 10 ? 3 : -9);                            // [1, 12]
+    return std::tuple<Int, unsigned, unsigned>(y + (m <= 2), m, d);
+}
+
+//! Returns day of week in civil calendar [0, 6] -> [Sun, Sat]
+//! Preconditions:  z is number of days since 1970-01-01 and is in the range:
+//!                   [numeric_limits<Int>::min(), numeric_limits<Int>::max()-4].
+//! Howard Hinnant
+//! chrono-Compatible Low-Level Date Algorithms
+//! http://home.roadrunner.com/~hinnant/date_algorithms.html
+template <class Int>
+// constexpr c++14
+unsigned weekday_from_days(Int z) noexcept
+{
+    return static_cast<unsigned>(z >= -4 ? (z+4) % 7 : (z+5) % 7 + 6);
+}
+
+//! Howard Hinnant
+//! chrono-Compatible Low-Level Date Algorithms
+//! http://home.roadrunner.com/~hinnant/date_algorithms.html
+template <class To, class Rep, class Period>
+To round_down(const std::chrono::duration<Rep, Period>& d)
+{
+    To t = std::chrono::duration_cast<To>(d);
+    if (t > d)
+        --t;
+    return t;
+}
+
+}
+
+//! Converts std::chrono::time_point<system_clock> to
+//! std::tm in UTC. Great thanks to Howard Hinnant,
+//! http://stackoverflow.com/a/16784256/1326885
+//! FIXME: leap seconds 
+//! (convert by the table at
+//! http://en.wikipedia.org/wiki/Leap_seconds)
+template <class Duration>
+std::tm make_utc_tm(
+  std::chrono::time_point
+    <std::chrono::system_clock, Duration> tp
+)
+{
+    using namespace std;
+    using namespace std::chrono;
+    using days = duration
+      <int, ratio_multiply<hours::period, ratio<24>>>;
+    // t is time duration since 1970-01-01
+    Duration t = tp.time_since_epoch();
+    // d is days since 1970-01-01
+    days d = howard_hinnant::round_down<days>(t);
+    // t is now time duration since midnight of day d
+    t -= d;
+    // break d down into year/month/day
+    int year;
+    unsigned month;
+    unsigned day;
+    std::tie(year, month, day) = 
+      howard_hinnant::civil_from_days(d.count());
+    // start filling in the tm with calendar info
+    std::tm tm = {0};
+    tm.tm_year = year - 1900;
+    tm.tm_mon = month - 1;
+    tm.tm_mday = day;
+    tm.tm_wday = howard_hinnant::weekday_from_days(d.count());
+    tm.tm_yday = d.count() - howard_hinnant::days_from_civil(year, 1, 1);
+    // Fill in the time
+    tm.tm_hour = duration_cast<hours>(t).count();
+    t -= hours(tm.tm_hour);
+    tm.tm_min = duration_cast<minutes>(t).count();
+    t -= minutes(tm.tm_min);
+    tm.tm_sec = duration_cast<seconds>(t).count();
+    return tm;
+}
+
+}
 
 //! seconds from the last midnight
 template < 
@@ -34,6 +191,49 @@ TDur seconds_since_midnight
       (time_point.time_since_epoch())
         % std::chrono::hours(24);
 }
+
+#if 0 // TODO
+
+template<
+  class CharT, 
+  class Clock,
+  class Duration = typename Clock::duration,
+  uint8_t N = 0
+>
+struct put_time_t
+{
+};
+
+std::ostream&
+operator<< (std::ostream& out, put_time_t&& t)
+{
+  
+}
+
+template <class charT, class traits>
+void f(basic_ios<charT, traits>& str, const struct tm* tmb, const charT* fmt) {
+  typedef ostreambuf_iterator<charT, traits> Iter;
+  typedef time_put<charT, Iter> TimePut;
+  const TimePut& tp = use_facet<TimePut>(str.getloc());
+  const Iter end = tp.put(Iter(str.rdbuf()), str, str.fill(), tmb,
+    fmt, fmt + traits::length(fmt));
+  if (end.failed())
+    str.setstate(ios_base::badbit);
+}
+
+template<
+  class CharT, 
+  class Clock,
+  class Duration = typename Clock::duration,
+  uint8_t N = 0
+>
+put_time_t put_time(
+  const time_point& time, 
+  const char(&format)[N]
+);
+
+#endif
+
 
 namespace types {
 
