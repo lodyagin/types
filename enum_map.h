0514038bd18a781562faff4771c2f36fe665d399
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-08-19 17:26:40 +0000

    enum_map.h is added

---
+++ enum_map.h
@@ -0,0 +1,49 @@
+// -*-coding: mule-utf-8-unix; fill-column: 58; -*-
+/**
+ * @file
+ * A map which can mix various enumerate<...> as a key.
+ *
+ * @author Sergei Lodyagin
+ */
+
+
+#ifndef TYPES_ENUM_MAP_H
+#define TYPES_ENUM_MAP_H
+
+//#include <type_traits>
+#include "types/enum.h"
+
+namespace types {
+
+template<
+  class T,
+  template<class...> class Map,
+  class... MapArgs
+>
+class enum_map 
+  : public Map<std::type_index, T, MapArgs...>
+{
+  // NB no data members
+  using map = Map<std::type_index, T, MapArgs...>;
+
+public:
+  //template<class... EnumVal>
+  //struct selector_t;
+
+  template<class Enum>
+  T& operator[](Enum e)
+  {
+    return map::operator[](enum_type_index<Enum>(e));
+  }
+
+  /*template<class... EnumVal>
+  selector_t<EnumVal...>&&  // protect storing?
+  select()
+  {
+    return selector_t<EnumVal...>(*this);
+  }*/
+};
+
+} // types
+
+#endif
